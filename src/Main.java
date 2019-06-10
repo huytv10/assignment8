cef7792259578f7bc03d65c04e2e544f024b9e3d
to van huy <huytv10@fsoft.com.vn>
2019-06-10 22:20:28 +0700

    assignment8: add code

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Calendar;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,8 +15,14 @@ public class Main {
                 String second = (calendar.getTime().getSeconds() > 9) ?
                         "" + calendar.getTime().getSeconds() + ""
                         : "0" + calendar.getTime().getSeconds();
+
+                Date date = new Date();
+
+                System.out.printf("%1$s %2$td %2$tB %2$tY ", "date:", date);
+
                 System.out.println(hour + ":" + minute + ":" + second);
                 Thread.sleep(1000);
+
             }
         } catch (InterruptedException e) {
             e.printStackTrace();
